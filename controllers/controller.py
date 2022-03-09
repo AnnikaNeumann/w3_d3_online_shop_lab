a3b9d9713e0458f749a0a4202f0b8814e1018379
Jacob <callaghanjacob@gmail.com>
2022-03-09 15:23:00 +0000

    extension complete

--- controllers/controller.py
+++ controllers/controller.py
@@ -6,6 +6,7 @@ from models.order_list import orders
 def index():
     return render_template('index.html', title = "Home", orders=orders)
 
-# @app.route('orders/1')
-# def get():
-#     return render_template("order.html", title = "Order", order = orders[0])
+@app.route('/orders/<index>')
+def get_order(index):
+    order = orders[int(index)]
+    return render_template("order.html", title = index , order = order)
