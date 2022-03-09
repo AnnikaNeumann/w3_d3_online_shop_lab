f4ac726a51d6c5f8750575bbecbe151b82ac14ae
Jacob <callaghanjacob@gmail.com>
2022-03-09 13:32:27 +0000

    basic file set up complete, order class added

---
+++ models/order.py
@@ -0,0 +1,6 @@
+class Order:
+    def __init__(self, customer_name, order_date, quantity, scooter_name):
+        self.customer_name = customer_name
+        self.order_date = order_date
+        self.quantity = quantity
+        self.scooter_name = scooter_name
---
+++ models/order_list.py
@@ -0,0 +1,6 @@
+from models.order import Order
+
+order_1 = Order("Jacob", "09/03/2022", 2, "Vespa")
+order_2 = Order("Annika", "09/03/2022", 2, "Piaggio")
+
+orders = [order_1, order_2]
