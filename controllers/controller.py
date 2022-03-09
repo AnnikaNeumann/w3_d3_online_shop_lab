c4c2bba21cf936c3dba5cced82f7faf6c1d51ca1
Jacob <callaghanjacob@gmail.com>
2022-03-09 14:56:31 +0000

    extension link complete

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,7 +1,11 @@
-from flask import render_template
+from flask import render_template, url_for
 from app import app
 from models.order_list import orders
 
 @app.route('/orders')
 def index():
     return render_template('index.html', title = "Home", orders=orders)
+
+# @app.route('orders/1')
+# def get():
+#     return render_template("order.html", title = "Order", order = orders[0])
--- models/order_list.py
+++ models/order_list.py
@@ -2,5 +2,6 @@ from models.order import Order
 
 order_1 = Order("Jacob", "09/03/2022", 2, "Vespa")
 order_2 = Order("Annika", "09/03/2022", 2, "Piaggio")
+order_3 = Order("John", "09/03/2022", 3, "Honda")
 
-orders = [order_1, order_2]
+orders = [order_1, order_2, order_3]
